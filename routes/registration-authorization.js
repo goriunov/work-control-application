bcc2bdf98a52ddd6e82f6373ffdff175834cf5e7
goriunovd <gotiunovd@gmail.com>
2016-11-14 22:58:50 +1300

    Add password hash

--- routes/registration-authorization.js
+++ routes/registration-authorization.js
@@ -2,17 +2,13 @@ var express = require('express');
 var router = express.Router();
 var User = require('../mongoose_model/user');
 var jwt = require('jsonwebtoken');
+var passworHash = require('password-hash');
 
 
 router.post('/registration' , function(req, res ,next){
-    console.log(req.body.email);
-    console.log(req.body.password);
-    console.log(req.body.phoneNumber);
-    console.log(req.body.firstName);
-    console.log(req.body.lastName);
     var user = new User({
         'email': req.body.email,
-        'password': req.body.password,
+        'password': passworHash.generate(req.body.password),
         'phoneNumber': req.body.phoneNumber,
         'firstName': req.body.firstName,
         'lastName': req.body.lastName
@@ -45,7 +41,7 @@ router.post('/sign-in', function(req , res ,next){
                 err: 'Wrong password or email'
             });
         }
-        if(response.password != req.body.password){
+        if(!passworHash.verify(req.body.password , response.password)){
             return res.status(403).json({
                 message: 'Some thing went wrong !',
                 err: 'Wrong password or email'
