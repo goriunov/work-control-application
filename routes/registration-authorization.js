873519f10a07ab0102bc6e8408dd30cafe7ab65f
goriunov <goriunovd@gmail.com>
2017-01-04 20:53:11 -0800

    (check):

--- routes/registration-authorization.js
+++ routes/registration-authorization.js
@@ -15,7 +15,7 @@ router.post('/registration' , function(req, res ,next){
         'lastName': req.body.lastName,
         'break': '30',
         'rate': '15',
-        'admin': false
+        'admin': true
     });
 
     user.save(function(err , response){
