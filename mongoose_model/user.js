9167a99900e4a06d9d2ffbea74010ce789fd7021
goriunov <goriunovd@gmail.com>
2017-01-06 15:41:43 +1300

    (chnage phoneNumber int to str)

--- mongoose_model/user.js
+++ mongoose_model/user.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var user = new Schema({
     'email': {type: String ,required: true , unique: true},
     'password': {type: String , required: true},
-    'phoneNumber': {type: Number , required: true},
+    'phoneNumber': {type: String, required: true},
     'firstName': {type: String , required: true},
     'lastName':{type: String , required :true},
     'admin': {type: Boolean , required: true},
