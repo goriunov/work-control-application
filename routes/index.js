371d1fbbb88da717b592b1bd56e0061982155dfe
Dmitrii Goriunov <goriunovd@gmail.com>
2018-01-30 18:06:39 +1300

    CHnage text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/' , function(req ,res ,next){
-    res.write('Website is in Work');
+    res.write('Website is in Work')
 });
 
 module.exports = router;
